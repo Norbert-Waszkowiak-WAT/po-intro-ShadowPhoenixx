cb6c07b072859c011e3f0e5816bde9be50ea4ee7
ShadowPhoenixx <104324854+ShadowPhoenixx@users.noreply.github.com>
2025-11-13 10:11:52 +0000

    cokolwiek

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class Point {
+public:
+int x, y;
+    void print() {  
+    cout << "Point(" << x << ", " << y << ")" << endl;
+    }
+};
+
+int main() {
+    Point p;
+    p1.x = 1;
+    p1.y = 2;
+    p1.print();
+    Point p2;
+    p2.x = 6;
+    p2.y = 7;
+    p2.print();
+}
+# g++ main.cpp -o main
+# ./main
